f8f6f1c23e2e4cb615d407f5b175f07843a1623a
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-12-10 16:23:03 +0500

    User Muhammad Haris Javed saved changes to main.py at 12/10/2024, 11:23:02 UTC time.

--- f2e5e8d7-41a5-499d-b218-3b8fb4d50a71/main.py
+++ f2e5e8d7-41a5-499d-b218-3b8fb4d50a71/main.py
@@ -1,1 +1,17 @@
-#Type code here
+#Doing shorting here
+from surmount.base_class import Strategy, TargetAllocation
+
+class TradingStrategy(Strategy):
+    @property
+    def assets(self):
+        return ["GOOGL"]
+
+    @property
+    def interval(self):
+        return "1day"
+
+    def run(self, data):
+        holdings = data["holdings"]
+        data = data["ohlcv"]
+  
+        return TargetAllocation({"GOOGL": -0.5})
